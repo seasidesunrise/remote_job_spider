c26f2dbc5f3df16b2ac595ab4f46a191ac0babdd
汪召军 <wangnewton@163.com>
2023-01-27 11:14:13 +0800

    检查了启动进程脚本

--- bash/cron_dy_job_spider.sh
+++ bash/cron_dy_job_spider.sh
@@ -6,5 +6,13 @@ dir=$(pwd)
 cd ${dir}/../
 export PYTHONPATH=${dir}/..:$PYTHONPATH
 
-/usr/local/bin/python3 电鸭/dy_spider.py $* >> /tmp/remote_job_spider.log 2>&1 &
-/usr/local/bin/python3 实现网/shixian_spider.py $* >> /tmp/remote_job_spider.log 2>&1 &
+# 判断进程是否存在，如果存在则
+dy_count=`ps -ef|grep dy_spider.py|grep -v grep | wc -l`
+if [ 0 == dy_count ];then
+  /usr/local/bin/python3 电鸭/dy_spider.py $* >> /tmp/remote_job_spider.log 2>&1 &
+fi
+
+sx_count=`ps -ef|grep shixian_spider.py|grep -v grep | wc -l`
+if [ 0 == sx_count ];then
+  /usr/local/bin/python3 实现网/shixian_spider.py $* >> /tmp/remote_job_spider.log 2>&1 &
+fi
